78cceab7d5fabf92f14aaf84e86c706128af4d6c
groselt <23747196+groselt@users.noreply.github.com>
2020-12-03 16:43:21 +0000

    Clean up day01
    Probably slower, but less duplication

--- day01.py
+++ day01.py
@@ -1,29 +1,31 @@
 #! /usr/bin/env python3
 
+from typing import Set, Tuple
+
 from utils import get_raw_input
 
 
-def part1(entries):
+def find_two_sum(entries: Set[int], total: int) -> Tuple[int, int]:
     for e in entries:
-        other = 2020 - e
+        other = total - e
         if other in entries:
-            print(e, other)
-            return e * other
+            return (e, other)
 
 
-def part2(entries):
+def part1(entries: Set[int]):
+    pair = find_two_sum(entries, 2020)
+    return pair[0] * pair[1]
+
+
+def part2(entries: Set[int]):
     for e1 in entries:
-        for e2 in entries:
-            if e1 == e2:
-                continue
-            other = 2020 - e1 - e2
-            if other in entries:
-                print(e1, e2, other)
-                return e1 * e2 * other
+        pair = find_two_sum(entries - {e1}, 2020-e1)
+        if pair:
+            return e1 * pair[0] * pair[1]
 
 
 if __name__ == '__main__':
     raw_input = [int(x) for x in get_raw_input().split()]
-    entries = set(raw_input)
+    entries: Set[int] = set(raw_input)
     print(part1(entries)) # 41979
     print(part2(entries)) # 193416912
