bebb2cb99e11a52ad0d9267da498361e836e18fc
groselt <23747196+groselt@users.noreply.github.com>
2020-12-03 16:43:32 +0000

    Clean up day02

--- day02.py
+++ day02.py
@@ -1,37 +1,46 @@
 #! /usr/bin/env python3
 
 from collections import Counter
+from typing import NamedTuple, List
 
 from utils import get_file_lines
 
 
-def part1(data) -> int:
+class PasswordInfo(NamedTuple):
+    int1: int
+    int2: int
+    char: str
+    password: str
+
+
+def parse_password_info(line: str) -> PasswordInfo:
+    raw_rule, password = line.split(': ')
+    int_rule, char = raw_rule.split()
+    int1, int2 = (int(i) for i in int_rule.split('-'))
+    return PasswordInfo(int1, int2, char, password)
+
+
+def part1(data: List[PasswordInfo]) -> int:
     result: int = 0
-    for entry in data:
-        raw_rule, password = entry
-        nr_rule, char = raw_rule.split()
-        min_nr, max_nr = (int(nr) for nr in nr_rule.split('-'))
-        counter = Counter(password)
-        if char in counter and min_nr <= counter[char] <= max_nr:
+    for info in data:
+        counter = Counter(info.password)
+        if info.char in counter and info.int1 <= counter[info.char] <= info.int2:
             result += 1
     return result
 
 
-def part2(data) -> int:
+def part2(data: List[PasswordInfo]) -> int:
     result: int = 0
-    for entry in data:
-        raw_rule, password = entry
-        pos_rule, char = raw_rule.split()
-        pos1, pos2 = (int(nr) for nr in pos_rule.split('-'))
-        password_len = len(password)
-        match1 = pos1 <= password_len and password[pos1-1] == char
-        match2 = pos2 <= password_len and password[pos2-1] == char
+    for info in data:
+        password_len = len(info.password)
+        match1 = info.int1 <= password_len and info.password[info.int1-1] == info.char
+        match2 = info.int2 <= password_len and info.password[info.int2-1] == info.char
         if match1 ^ match2:
-                result += 1
+            result += 1
     return result
 
 
 if __name__ == '__main__':
-    raw_input = [line.split(': ') for line in get_file_lines()]
-    print(part1(raw_input)) # 474
-    print(part2(raw_input)) # 745
+    data = [parse_password_info(line) for line in get_file_lines()]
+    print(part1(data)) # 474
+    print(part2(data)) # 745
