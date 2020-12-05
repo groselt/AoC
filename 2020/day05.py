752a86059213a10a8c463099d1db25eba9b49952
groselt <23747196+groselt@users.noreply.github.com>
2020-12-05 09:50:57 +0000

    Clean up

--- 2020/day05.py
+++ 2020/day05.py
@@ -1,34 +1,33 @@
 #! /usr/bin/env python3
 
-from typing import List
+from typing import Optional, Set
 
 from utils import get_file_lines
 
 
 def parse_ticket(raw_ticket: str) -> int:
-    binary = raw_ticket.replace('F','0').replace('B','1').replace('L','0').replace('R','1')
+    table = str.maketrans('FBLR', '0101')
+    binary = raw_ticket.translate(table)
     return int(binary, 2)
 
 
-def part1(tickets: List[int]) -> int:
+def part1(tickets: Set[int]) -> int:
     return max(tickets)
     
 
-def part2(tickets: List[int]) -> int:
-    ticket_set = set(tickets)
-    x = min(ticket_set) + 1
-    i = 1
-    while i <= len(tickets):
-        if x not in ticket_set and \
-            x-1 in ticket_set and \
-            x+1 in ticket_set:
+def part2(tickets: Set[int]) -> Optional[int]:
+    def is_gap(x: int) -> bool:
+        return x not in tickets and \
+            x-1 in tickets and \
+            x+1 in tickets
+
+    for x in range(min(tickets)+1, max(tickets)):
+        if is_gap(x):
             return x
-        x += 1
-        i += 1
-    return -1
+    return None
 
 
 if __name__ == '__main__':
-    data = [parse_ticket(t) for t in get_file_lines()]
+    data = set(parse_ticket(t) for t in get_file_lines())
     print(part1(data))  # 911
     print(part2(data))  # 629
