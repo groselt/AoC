4ef20cacc2e56e77a0236d089334c159a56d4cd4
groselt <23747196+groselt@users.noreply.github.com>
2020-12-06 12:46:04 +0000

    Clean up

--- 2020/day06.py
+++ 2020/day06.py
@@ -1,44 +1,36 @@
 #! /usr/bin/env python3
 
 from collections import Counter
+import itertools
 from typing import List
 
 from utils import get_file_lines
 
 
-def group_data(raw_data: List[str]) -> List[str]:
-    result = []
-    group = []
-    for line in raw_data:
+def group_data(raw_lines: List[str]) -> List[List[str]]:
+    result: List[List[str]] = [[]]
+    for line in raw_lines:
         if not line:
-            result.append(group)
-            group = []
+            result.append([])
         else:
-            group.append(line)
-    if group:
-        result.append(group)
+            result[-1].append(line)
     return result
 
 
 def part1(groups: List[List[str]]) -> int:
-    result = 0
-    for group in groups:
-        answers = [x for g in group for x in g]
-        result += len(Counter(answers))
-    return result
-    
+    return sum(len(Counter(itertools.chain.from_iterable(g))) for g in groups)
+
 
 def part2(groups: List[List[str]]) -> int:
-    result = 0
+    result: int = 0
     for group in groups:
-        answers = [x for g in group for x in g]
-        counts = Counter(answers)
-        result += sum(1 for v in counts.values() if v == len(group))
+        counts = Counter(itertools.chain.from_iterable(group))
+        result += sum(v == len(group) for v in counts.values())
     return result
 
 
 if __name__ == '__main__':
     raw_data = get_file_lines()
-    group_data = group_data(raw_data)
-    print(part1(group_data))  # 6809
-    print(part2(group_data))  # 3394
+    grouped_data = group_data(raw_data)
+    print(part1(grouped_data))  # 6809
+    print(part2(grouped_data))  # 3394
