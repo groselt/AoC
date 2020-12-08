7e048687ca0b929ed21ab2774ae726dfd48afd22
groselt <23747196+groselt@users.noreply.github.com>
2020-12-08 06:53:30 +0000

    Clean up

--- 2020/day08.py
+++ 2020/day08.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from typing import Dict, List, NamedTuple, Tuple
+from typing import List, NamedTuple, Optional, Set
 
 from utils import get_file_lines
 
@@ -11,19 +11,19 @@ class Instruction(NamedTuple):
 
 
 class Computer:
-    def __init__(self, program):
+    def __init__(self, program: List[str]):
         self.instructions = self.parse(program)
         self.reset()
 
     @staticmethod
-    def parse(program):
+    def parse(program: List[str]) -> List[Instruction]:
         instructions = []
         for line in program:
             words = line.split()
             instructions.append(Instruction(words[0], int(words[1])))
         return instructions
 
-    def reset(self):
+    def reset(self) -> None:
         self.ip = 0  # instruction pointer
         self.accumulator = 0
 
@@ -41,7 +41,7 @@ class Computer:
 
 
 def run_to_loop_start(computer: Computer) -> bool:
-    executed = set()
+    executed: Set[int] = set()
     while computer.ip not in executed and computer.ip < len(computer.instructions):
         executed.add(computer.ip)
         computer.step()
@@ -53,7 +53,7 @@ def part1(computer: Computer) -> int:
     return computer.accumulator
 
 
-def part2(computer: Computer) -> int:
+def part2(computer: Computer) -> Optional[int]:
     def make_new_instruction(old: Instruction) -> Instruction:
         new_operation = 'jmp' if old.operation == 'nop' else 'nop'
         return Instruction(new_operation, old.param)
@@ -66,10 +66,11 @@ def part2(computer: Computer) -> int:
             if not run_to_loop_start(computer):
                 return computer.accumulator
             computer.instructions[i] = old_instruction
+    return None
 
 
 if __name__ == '__main__':
     raw_instructions = get_file_lines()
-    computer = Computer(raw_instructions)
-    print(part1(computer))  # 2051
-    print(part2(computer))  # 2304
+    main_computer = Computer(raw_instructions)
+    print(part1(main_computer))  # 2051
+    print(part2(main_computer))  # 2304
