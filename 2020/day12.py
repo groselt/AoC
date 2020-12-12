fce645269adab58878f3034684c95b1ea22b5199
groselt <23747196+groselt@users.noreply.github.com>
2020-12-12 18:01:42 +0000

    Clean up

--- 2020/day12.py
+++ 2020/day12.py
@@ -1,10 +1,11 @@
 #! /usr/bin/env python3
 
-from itertools import chain
-from typing import List, NamedTuple
+from typing import List, NamedTuple, Tuple
 
 from utils import get_file_lines
 
+
+Matrix2D = Tuple[Tuple[int, int], Tuple[int, int]]
 
 DIRECTION = {
     'N': complex(0, 1),
@@ -24,51 +25,31 @@ class Instruction(NamedTuple):
     parameter: int
 
 
-class Navigation1:
-    def __init__(self):
-        self.position = complex(0, 0)
-        self.facing = DIRECTION['E']
-
-    def distance(self):
-        return abs(self.position.real) + abs(self.position.imag)
-
-    def apply(self, instruction: Instruction) -> None:
-        if instruction.action == 'F':
-            self.position += instruction.parameter * self.facing
-        elif instruction.action in DIRECTION.keys():
-            self.position += instruction.parameter * DIRECTION[instruction.action]
-        else:
-            self.rotate(ROTATION_90[instruction.action], instruction.parameter//90)
-
-    def rotate(self, matrix, times: int):
-        for _ in range(times % 4):
-            self.facing = complex(
-                            self.facing.real*matrix[0][0] + self.facing.imag*matrix[0][1],
-                            self.facing.real*matrix[1][0] + self.facing.imag*matrix[1][1]
-                            )
-
-
-class Navigation2:
-    def __init__(self, waypoint: complex):
+class Navigation:
+    def __init__(self, waypoint: complex, loose_waypoint: bool):
+        '''If `loose_waypoint` is True, position won't change
+           when wind direction command is received'''
         self.position = complex(0, 0)
         self.waypoint = waypoint
+        self.loose_waypoint = loose_waypoint
 
-    def distance(self):
-        return abs(self.position.real) + abs(self.position.imag)
+    def distance(self) -> int:
+        return int(abs(self.position.real) + abs(self.position.imag))
 
     def apply(self, instruction: Instruction) -> None:
         if instruction.action == 'F':
             translation = instruction.parameter * (self.waypoint - self.position)
             self.position += translation
             self.waypoint += translation
-        elif instruction.action in DIRECTION.keys():
-            translation = instruction.parameter * DIRECTION[instruction.action]
-            # self.position += translation
-            self.waypoint += translation
-        else:
+        elif instruction.action in ROTATION_90.keys():
             self.rotate(ROTATION_90[instruction.action], instruction.parameter//90)
+        else:
+            wind_direction_change = instruction.parameter * DIRECTION[instruction.action]
+            self.waypoint += wind_direction_change
+            if not self.loose_waypoint:
+                self.position += wind_direction_change
 
-    def rotate(self, matrix, times: int):
+    def rotate(self, matrix: Matrix2D, times: int):
         for _ in range(times % 4):
             vector = self.waypoint - self.position
             self.waypoint = self.position + complex(
@@ -82,14 +63,14 @@ def parse_instruction(raw_instruction: str) -> Instruction:
 
 
 def part1(instructions: List[Instruction]) -> int:
-    navigation = Navigation1()
+    navigation = Navigation(DIRECTION['E'], loose_waypoint=False)
     for instruction in instructions:
         navigation.apply(instruction)
     return navigation.distance()
 
 
 def part2(instructions: List[Instruction]) -> int:
-    navigation = Navigation2(10+1j)
+    navigation = Navigation(10+1j, loose_waypoint=True)
     for instruction in instructions:
         navigation.apply(instruction)
     return navigation.distance()
