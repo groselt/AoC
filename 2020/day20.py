b3bb6b9505bd836b09f5831a65086bb781640ec4
groselt <23747196+groselt@users.noreply.github.com>
2020-12-21 10:26:11 +0000

    Minor clean-up

--- 2020/day20.py
+++ 2020/day20.py
@@ -2,19 +2,25 @@
 
 from collections import defaultdict
 import math
-from typing import Dict, Iterable, List, Set
+from typing import Dict, Iterable, List, Callable, Set
 
 from utils import get_file_lines
 
+PUZZLE_WIDTH = 12
+MAX_FLIPS = 2
+MAX_ROTATIONS = 4
 
-TOP = 0
-LEFT = 1
-BOTTOM = 2
-RIGHT = 3
+GetTileSide = Callable[['Tile'], str]
+GetTileSide1 = Callable[['Tile'], str]
 
 class Tile:
-    def __init__(self, id: int, data: List[str]):
-        self.id = id
+    TOP = 1
+    RIGHT = 2
+    BOTTOM = 3
+    LEFT = 4
+
+    def __init__(self, _id: int, data: List[str]):
+        self.id = _id
         self.data = data
 
     @property
@@ -27,11 +33,23 @@ class Tile:
 
     @property
     def left(self) -> str:
-        return self._get_column(0)
+        return get_column(self.data, 0)
 
     @property
     def right(self) -> str:
-        return self._get_column(len(self.data)-1)
+        return get_column(self.data, len(self.data)-1)
+
+    def get_side(self, direction: int) -> str:
+        if direction == Tile.TOP:
+            return self.top
+        if direction == Tile.RIGHT:
+            return self.right
+        if direction == Tile.BOTTOM:
+            return self.bottom
+        if direction == Tile.LEFT:
+            return self.left
+        raise RuntimeError(f'Unhandled direction {direction}')
+
 
     def can_connect(self, other: 'Tile') -> bool:
         other_orienations = other.generate_all_side_orienations()
@@ -40,34 +58,41 @@ class Tile:
                 or self.right in other_orienations \
                     or self.bottom in other_orienations
 
-    def _get_column(self, col: int) -> str:
-        return ''.join(row[col] for row in self.data)
-
     def generate_all_side_orienations(self) -> Set[str]:
         return {
-            self.top, self.top[::-1], 
+            self.top, self.top[::-1],
             self.right, self.right[::-1],
             self.bottom, self.bottom[::-1],
             self.left, self.left[::-1]
         }
 
     def flip(self) -> None:
-        for i, row in enumerate(self.data):
-            self.data[i] = row[::-1]
+        flip(self.data)
 
     def rotate(self) -> None:
-        data: List[str] = []
-        for col in range(len(self.data[0])):
-            data.append(self._get_column(col)[::-1])
-        self.data = data
+        self.data = rotated(self.data)
+
+    def orientate_and_connect(self, side: int, other_side: str) -> bool:
+        for _ in self.get_all_orientations():
+            if self.get_side(side) == other_side:
+                return True
+        return False
+
+    def get_all_orientations(self) -> Iterable:
+        for _ in range(MAX_FLIPS):
+            for _ in range(MAX_ROTATIONS):
+                yield self
+                self.rotate()
+            self.flip()
 
 
 def parse_pieces(data: List[str]) -> Iterable[Tile]:
-    current_id, current_data = None, []
+    current_id: int = -1
+    current_data: List[str] = []
     for line in data:
         if not line:
             yield Tile(current_id, current_data)
-            current_id, current_data = None, []
+            current_id, current_data = -1, []
         elif line.startswith('Tile'):
             current_id = int(line[5:-1])
         else:
@@ -93,7 +118,7 @@ def rotated(data: List[str]) -> List[str]:
 
 
 def get_connections(tiles: Set[Tile]) -> Dict[int, List[int]]:
-    connections: Dict[int, List[int]] = defaultdict(int)
+    connections: Dict[int, List[int]] = defaultdict(list)
     for tile in tiles:
         connections[tile.id] = []
         for other in tiles:
@@ -102,12 +127,12 @@ def get_connections(tiles: Set[Tile]) -> Dict[int, List[int]]:
             if tile.can_connect(other):
                 connections[tile.id].append(other.id)
     return connections
-    
 
-def orientate_top_left(id: int, tiles: Dict[int, Tile], connections: Dict[int, List[int]]) -> None:
+
+def orientate_top_left(_id: int, tiles: Dict[int, Tile], connections: Dict[int, List[int]]) -> None:
     def all_neigbour_sides():
         sides = set()
-        for neighbour in connections[id]:
+        for neighbour in connections[_id]:
             for side in tiles[neighbour].generate_all_side_orienations():
                 sides.add(side)
         return sides
@@ -115,57 +140,22 @@ def orientate_top_left(id: int, tiles: Dict[int, Tile], connections: Dict[int, L
     def is_orientated(tile: Tile, other_sides: Set[str]):
         return tile.left not in other_sides and tile.top not in other_sides
 
-    flips = 2
-    rotations = 4
-    tile = tiles[id]
-    print(connections[id])
-    for y in range(len(tile.data)):
-        print(tile.data[y], '\t', tiles[connections[id][0]].data[y], '\t', tiles[connections[id][1]].data[y])
-    print()
+    tile = tiles[_id]
     other_sides = all_neigbour_sides()
-    for _ in range(flips):
-        for __ in range(rotations):
-            print(tile.top)
-            if is_orientated(tile, other_sides):
-                return
-            tile.rotate()
-        print('flip')
-        tile.flip()
+    for _ in tile.get_all_orientations():
+        if is_orientated(tile, other_sides):
+            return
     raise RuntimeError('Could not orientate top left tile')
-
-
-def orientate_and_connect_left(left_side: str, tile: Tile) -> bool:
-    flips = 2
-    rotations = 4
-    for _ in range(flips):
-        for __ in range(rotations):
-            if tile.left == left_side:
-                return True
-            tile.rotate()
-        tile.flip()
-    return False
-
-
-def orientate_and_connect_top(top_side: str, tile: Tile) -> bool:
-    flips = 2
-    rotations = 4
-    for _ in range(flips):
-        for __ in range(rotations):
-            if tile.top == top_side:
-                return True
-            tile.rotate()
-        tile.flip()
-    return False
 
 
 def build_map(tiles: Dict[int, Tile], puzzle: List[List[int]]):
     result = []
     tile_row_count = len(list(tiles.values())[0].data)
-    for row in range(len(puzzle)):
+    for puzzle_row in puzzle:
         for tile_row in range(1, tile_row_count-1):
             map_row = ''
             for col in range(len(puzzle[0])):
-                tile = tiles[puzzle[row][col]]
+                tile = tiles[puzzle_row[col]]
                 map_row += tile.data[tile_row][1:-1]
             result.append(map_row)
     return result
@@ -212,33 +202,40 @@ def mark_mask(full_map: List[str], mask: List[str]):
                 mark(row, col)
 
 
+def get_all_map_orientations(data: List[str]) -> Iterable[List[str]]:
+    for _ in range(MAX_FLIPS):
+        for _ in range(MAX_ROTATIONS):
+            yield data
+            data = rotated(data)
+        flip(data)
+
+
 def part1(tiles: Dict[int, Tile]) -> int:
-    connections = get_connections(tiles.values())
+    connections = get_connections(set(tiles.values()))
     return math.prod(id for id, neighbours in connections.items() if len(neighbours) == 2)
 
 
 def part2(tiles: Dict[int, Tile]) -> int:
-    LEN = 12
-    connections = get_connections(tiles.values())
+    connections = get_connections(set(tiles.values()))
     first_corner = next(id for id, neighbours in connections.items() if len(neighbours) == 2)
     orientate_top_left(first_corner, tiles, connections)
     puzzle = [[first_corner]]
 
     # Build first column
     last_id = first_corner
-    for row in range(LEN-1):
+    for row in range(PUZZLE_WIDTH-1):
         for bottom in connections[last_id]:
-            if orientate_and_connect_top(tiles[last_id].bottom, tiles[bottom]):
+            if tiles[bottom].orientate_and_connect(Tile.TOP, tiles[last_id].bottom):
                 puzzle.append([bottom])
                 last_id = bottom
                 break
 
     # Build rows
-    for row in range(LEN):
+    for row in range(PUZZLE_WIDTH):
         left_id = puzzle[row][0]
-        for col in range(LEN-1):
+        for _ in range(PUZZLE_WIDTH-1):
             for right in connections[left_id]:
-                if orientate_and_connect_left(tiles[left_id].right, tiles[right]):
+                if tiles[right].orientate_and_connect(Tile.LEFT, tiles[left_id].right):
                     puzzle[row].append(right)
                     left_id = right
 
@@ -248,31 +245,19 @@ def part2(tiles: Dict[int, Tile]) -> int:
             '#    ##    ##    ###',
             ' #  #  #  #  #  #   ']
 
-    flips = 2
-    rotations = 4
     map_orientations = dict()
-    for _ in range(flips):
-        for __ in range(rotations):
-            print(full_map[0][:30])
-            count = get_mask_count(full_map, mask)
-            map_orientations[count] = full_map.copy()
-            print(count)
-            full_map = rotated(full_map)
-        flip(full_map)
+    for new_map in get_all_map_orientations(full_map):
+        monster_count = get_mask_count(new_map, mask)
+        map_orientations[monster_count] = new_map.copy()
 
-    max_monsters = max(map_orientations.keys())
-    full_map = map_orientations[max_monsters]
+    monster_count = max(map_orientations.keys())
+    full_map = map_orientations[monster_count]
     mark_mask(full_map, mask)
-    for row in full_map:
-        print(row)
     return sum(c == '#' for row in full_map for c in row)
-
-
-
 
 
 if __name__ == '__main__':
     all_pieces = {piece.id: piece for piece in parse_pieces(get_file_lines())}
 
-    # print(part1(all_pieces))  # 18449208814679
+    print(part1(all_pieces))  # 18449208814679
     print(part2(all_pieces))  # 1559
